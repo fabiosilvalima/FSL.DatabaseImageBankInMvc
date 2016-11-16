88045a32d11be1e98c5f9049dd107fd530c2a7e4
Fabio Silva Lima <fabiosilvalima@terra.com.br>
2016-11-16 09:17:05 -0200

    primeira subida

---
+++ FSL.DatabaseImageBankInMvc/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(FSL.DatabaseImageBankInMvc.Startup))]
+namespace FSL.DatabaseImageBankInMvc
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
